7316e405e2d1465c4df38a1ff8bf9cdb7549e6c7
Logeshkumar <logesh07cbe@gmail.com>
2023-11-26 13:38:23 +0530

    profile page worked

--- works/src/Component/profile/profile.js
+++ works/src/Component/profile/profile.js
@@ -2,18 +2,46 @@
 import React from "react";
 import { useUser } from "../userContext/userContext";
 import AppBars from "../NavigationBar/Appbar";
-
+import ProfileCard from "./ProfileCard";
+import './ProfileCard.css'
+import { useEffect } from "react";
 const Profile = () => {
   const { user } = useUser();
 
   
+  useEffect(() => {
+    // Save original body background style
+    const originalBackground = document.body.style.background;
+    const originalBackgroundSize = document.body.style.backgroundSize;
 
+    // Set new body background style
+    document.body.style.background =
+      "url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c9a17937-8c0d-4a78-b75b-d9847af5b606/dg7ssyw-c79f2b26-1b69-4633-8cd6-4256bcca93eb.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2M5YTE3OTM3LThjMGQtNGE3OC1iNzViLWQ5ODQ3YWY1YjYwNlwvZGc3c3N5dy1jNzlmMmIyNi0xYjY5LTQ2MzMtOGNkNi00MjU2YmNjYTkzZWIuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.EO5d-A_h03cUY0q0fAsM8qlGBnPxiT2hybyl1VHA1ws) repeat";
+    document.body.style.backgroundSize = "cover";
+
+    // Reset to original body background style on component unmount
+    return () => {
+      document.body.style.background = originalBackground;
+      document.body.style.backgroundSize = originalBackgroundSize;
+    };
+  }, []);
   return (
     <div>
         <AppBars/>
-      <h1 style={{color:'greenyellow'}}>Welcome, {user.name}!</h1>
       
-      {/* Add more profile information or components as needed */}
+      <div className="background">
+			<div className="bg-pattern-top"></div>
+			<div className="bg-pattern-bottom"></div>
+
+			<ProfileCard
+				name={user.name}
+				age="28"
+				city="Nantes"
+				followers="666K"
+				likes="123K"
+				photos="614"
+			></ProfileCard>
+		</div>
     </div>
   );
 };
